03550172ec55538af87986f77d487ddda23f4cf3
ss-20 <sqldalmaker@gmail.com>
2022-11-26 12:47:30 +0200

    + task_schema.load

--- resources/task_resource.py
+++ resources/task_resource.py
@@ -1,11 +1,18 @@
 import flask
+from flask import Response
 from flask_restful import Resource
+from marshmallow import ValidationError, fields
 
 from app_marshmallow import ma
 from services.tasks_service import *
 
 
 class TaskSchema(ma().Schema):
+    t_date = fields.Str(required=True)
+    t_subject = fields.Str(required=True)
+    t_priority = fields.Int(required=True)
+    t_comments = fields.Str(required=False)
+
     class Meta:
         fields = ("t_id", "g_id", "t_date", "t_subject", "t_priority", "t_comments")
         model = Task
@@ -23,7 +30,14 @@ class TaskResource(Resource):
     @staticmethod
     def put(t_id):
         req_json = flask.request.json
-        update_task(t_id, req_json)
+        try:
+            data = task_schema.load(req_json)
+        except ValidationError as error:
+            return Response(
+                error.messages,
+                status=400,
+            )
+        update_task(t_id, data)
 
     @staticmethod
     def delete(t_id):
